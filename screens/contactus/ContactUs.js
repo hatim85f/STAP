429255f7b1f8529b0e372a5a0a1758dc52e0c592
hatim85f <50639901+hatim85f@users.noreply.github.com>
2024-04-30 21:06:57 +0400

    checking if updates applied

--- screens/contactus/ContactUs.js
+++ screens/contactus/ContactUs.js
@@ -6,7 +6,7 @@ import { useDispatch, useSelector } from "react-redux";
 import Colors from "../../constants/Colors";
 import { globalHeight, globalWidth } from "../../constants/globalWidth";
 import ContactsData from "./ContactsData";
-import MenuButton from "../../components/MenuButton";
+import MenuButton from "../../components/webComponents/menu/MenuButton";
 
 import * as formsActions from "../../store/contact/contactActions";
 import * as authActions from "../../store/auth/authActions";
